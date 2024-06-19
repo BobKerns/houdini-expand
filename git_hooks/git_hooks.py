13804e5cf2c16ff6fe97e43b653da46c2c134f54
Bob Kerns <1154903+BobKerns@users.noreply.github.com>
2024-06-18 19:04:37 -0700

    Set up git_hooks.py to run from source tree as well as install.

--- git_hooks/git_hooks.py
+++ git_hooks/git_hooks.py
@@ -10,7 +10,18 @@ if sys.version_info[0] != 3 or sys.version_info[1] < 12:
     print(f'This script requires Python 3.12 or later: {sys.version_info[1]}', file=sys.stderr)
     sys.exit(1)
 
-sys.path.insert(0, str(Path(__file__).parent))
+dir = Path(__file__).parent
+if (dir / 'git_hooks.py').is_file():
+    # We are being run from the git_hooks source directory.
+    dir = dir.parent
+elif (dir / 'git_hooks').is_dir():
+    # We are being run from an installation on the PATH.
+    pass
+else:
+    print(f'Cannot find the git_hooks package in {dir}', file=sys.stderr)
+    sys.exit(1)
+
+sys.path.insert(0, str(dir))
 
 from git_hooks.cmdline import cmdline
 
